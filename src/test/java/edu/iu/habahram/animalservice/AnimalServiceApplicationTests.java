64368baed180a2d0b4bd4bf3b614ef9f349ca575
hbahramian <habahram@iu.edu>
2024-01-23 19:58:01 -0500

    Initial commit

---
+++ src/test/java/edu/iu/habahram/animalservice/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.habahram.animalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
